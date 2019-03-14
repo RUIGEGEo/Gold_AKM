9e2100a8efebe12fac4a3352001acc63e9904394
rui <rui@guigu.com>
2019-03-14 21:19:19 +0800

    sss

---
+++ akm/src/main/java/com/pubg/akm/AkmApplication.java
@@ -0,0 +1,13 @@
+package com.pubg.akm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkmApplication.class, args);
+	}
+
+}
